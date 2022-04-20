e6432fd510ed48003b8d79f827f57567c4f07c49
Miguelonches <zap407@amerike.edu.mx>
2022-04-19 20:39:11 -0500

    Porterias

---
+++ Soccer/Assets/Scripts/Goal.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+  public Transform positionBall;
+  public Transform positionPlayer1;
+  public Transform positionPlayer2;
+  public Transform player1;
+  public Transform player2;
+  public Transform ball;
+  public int scorePlayer1;
+  public int scorePlayer2;
+  public int goal;
+  public TextMeshProUGUI uiScorePlayer1;
+  public TextMeshProUGUI uiScorePlayer2;
+  
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.CompareTag("Ball"))
+    {
+      if (goal == 1)
+      {
+        scorePlayer1++;
+        uiScorePlayer1.text = "Atlas -" + scorePlayer1.ToString();
+      }
+      else
+      {
+        scorePlayer2++;
+        uiScorePlayer2.text = scorePlayer2.ToString() + " - Poland";
+      }
+      ball.transform.position = positionBall.position;
+      player1.transform.position = positionPlayer1.position;
+      player2.transform.position = positionPlayer2.position;
+    }
+  }
+}
